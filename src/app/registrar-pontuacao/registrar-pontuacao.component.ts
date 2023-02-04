b28c90bec19145c3ab6ef2db4fddda24c9d6b38e
ronye <ronye.rocha@gmail.com>
2023-02-03 21:48:09 -0300

    router redirect

--- src/app/registrar-pontuacao/registrar-pontuacao.component.ts
+++ src/app/registrar-pontuacao/registrar-pontuacao.component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from '@angular/core';
 import { NgbDate } from '@ng-bootstrap/ng-bootstrap';
 import { ApiserviceService } from '../apiservice.service';
 import { NovaPontuacaoModel } from '../models/NovaPontuacaoModel';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-registrar-pontuacao',
@@ -10,15 +11,15 @@ import { NovaPontuacaoModel } from '../models/NovaPontuacaoModel';
 })
 export class RegistrarPontuacaoComponent {
 
-  constructor(private service: ApiserviceService) {}
+  constructor(private service: ApiserviceService, private router: Router) {}
 
   @Input() novaPontuacao: NovaPontuacaoModel = new NovaPontuacaoModel;
   @Input() selectedDate!: NgbDate;
 
   registrarPontuacao() {
-    this.novaPontuacao.DataPartida = new Date(this.selectedDate.year, this.selectedDate.month -1, this.selectedDate.day);
-    this.service.registrarPontuacao(this.novaPontuacao).subscribe(res => {
-      alert(res.toString());
+      this.novaPontuacao.DataPartida = new Date(this.selectedDate.year, this.selectedDate.month -1, this.selectedDate.day);
+      this.service.registrarPontuacao(this.novaPontuacao).subscribe(res => {
+      this.router.navigate(['/ranking']);
     })
   }
 }
